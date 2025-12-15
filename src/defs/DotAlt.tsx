c8ec84776d80f079ec2de73858ba643f8d7559fd
flavien <flavien@gensdeconfiance.com>
2025-12-15 15:35:04 +0100

    Update core to latest commit

---
+++ src/defs/DotAlt.tsx
@@ -0,0 +1,7 @@
+/* GENERATED FILE */
+import React, { ReactElement } from "react";
+import { IconWeight } from "../lib";
+
+export default new Map<IconWeight, ReactElement>([
+["bold", <><path fill="{color}" d="M144 128a16 16 0 1 1-32 0 16 16 0 0 1 32 0"/></>],["duotone", <><path fill="{color}" d="M144 128a16 16 0 1 1-32 0 16 16 0 0 1 32 0"/></>],["fill", <><path fill="{color}" d="M144 128a16 16 0 1 1-32 0 16 16 0 0 1 32 0"/></>],["light", <><path fill="{color}" d="M144 128a16 16 0 1 1-32 0 16 16 0 0 1 32 0"/></>],["regular", <><path fill="{color}" d="M144 128a16 16 0 1 1-32 0 16 16 0 0 1 32 0"/></>],["thin", <><path fill="{color}" d="M144 128a16 16 0 1 1-32 0 16 16 0 0 1 32 0"/></>]
+]);
